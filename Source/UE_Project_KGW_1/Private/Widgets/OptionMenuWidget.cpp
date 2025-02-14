d1fe6e89e55173a23159794b16abf25ca2259c63
gwo1004 <mynameiskgw@gmail.com>
2025-02-14 15:10:43 +0900

    [TEST]

--- Source/UE_Project_KGW_1/Private/Widgets/OptionMenuWidget.cpp
+++ Source/UE_Project_KGW_1/Private/Widgets/OptionMenuWidget.cpp
@@ -112,5 +112,10 @@ void UOptionMenuWidget::PopulateKeyBindings()
 
 		KeyWidget->OnKeyBindingUpdated.AddDynamic(this, &UOptionMenuWidget::HandleKeyBindingUpdated);
 		KeyBindingList->AddChild(KeyWidget);
+
+		// 무언가를 추가했습니다.
+		// 원격으로 보내서 작업물을 동기화할거에요
+		// Asset
+
 	}
 }
